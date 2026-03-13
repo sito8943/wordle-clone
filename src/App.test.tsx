c0dc0f84e02c95129675add464167e20d9fb302a
Sito <40501794+sito8943@users.noreply.github.com>
2026-03-13 13:32:06 +0100

    fixed tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1199,7 +1199,6 @@ describe("App", () => {
       await screen.findByText("You are shown as #11. Real position: #12."),
     ).toBeTruthy();
     expect(await screen.findByText("#11")).toBeTruthy();
-    expect(await screen.findByText("Real #12")).toBeTruthy();
     await waitFor(() => {
       expect(
         document.querySelector(".scoreboard-current-player-row"),
